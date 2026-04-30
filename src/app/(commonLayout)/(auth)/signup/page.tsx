f908010ddcb47f65d729937db42ead18755b61a9
SabbirNiyaz <sabbirlm10niyaz@gmail.com>
2026-05-01 02:59:45 +0600

    Fix Footer

--- src/app/(commonLayout)/(auth)/signup/page.tsx
+++ src/app/(commonLayout)/(auth)/signup/page.tsx
@@ -2,7 +2,7 @@
 export default function SignupPage() {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">Signup Page!</h1>
+            <h1 className="text-indigo-500 text-3xl font-bold underline">Signup Page!</h1>
         </div>
     );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,25 +1,36 @@
-
 import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-900 text-white py-10 px-6">
-        {/* BOTTOM BAR */}
-        <div className="mt-2 border-t pt-6 flex flex-col md:flex-row items-center justify-between gap-4 text-sm text-muted-foreground">
-          <p>© {new Date().getFullYear()}  E-commerce Management Web. All rights reserved.</p>
+    <footer className="border-t mt-10 bg-zinc-100 dark:bg-gray-900 text-gray-600 dark:text-gray-400">
 
-          <div className="flex gap-6">
-            <Link href="/terms" className="hover:text-primary">
-              Terms
-            </Link>
-            <Link href="/privacy" className="hover:text-primary">
-              Privacy
-            </Link>
-            <Link href="/cookies" className="hover:text-primary">
-              Cookies
-            </Link>
-          </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+
+        {/* LINKS */}
+        <div className="flex flex-col-2 sm:flex-row justify-center items-center gap-2 sm:gap-4 text-sm mb-4">
+          <Link href="/about" className="hover:underline">
+            About
+          </Link>
+          <Link href="/contact" className="hover:underline">
+            Contact
+          </Link>
+          <Link href="/privacy" className="hover:underline">
+            Privacy Policy
+          </Link>
+          <Link href="/help" className="hover:underline">
+            Help
+          </Link>
         </div>
+
+        {/* COPYRIGHT */}
+        <div className="flex justify-center gap-1 text-xs sm:text-sm">
+          &copy; {new Date().getFullYear()} All rights reserved | 
+          <Link href="/" className="hover:underline">
+            E-commerce Management Web
+          </Link>
+        </div>
+      </div>
+
     </footer>
   );
 }
